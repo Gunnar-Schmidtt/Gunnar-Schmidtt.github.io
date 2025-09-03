72a92f5ad79b572ec7172bb036c7806945b2959a
Gunnar-Schmidtt <gsorensenschmidt@gmail.com>
2025-09-03 10:26:22 -0500

    Add files via upload

--- Gunnar-Schmidtt-main/scripts/script.js
+++ Gunnar-Schmidtt-main/scripts/script.js
@@ -1,5 +1,16 @@
 
-document.addEventListener("DOMContentLoaded", function(e) {
-    console.log("we're so back");
-    alert("Hello")
+document
+  .addEventListener("close", () => {
+    console.log("we're so back")
+    // window.alert(hi);
   });
+
+function btnClicky() {
+
+document.getElementById('btnClicky'); btnClicky.addEventListener ('click'), btnClicky;
+  console.log('wadu heck')
+  .getElementById('btnClicky').style.tranfrom = 'rotate(15deg)'
+  // .querySelectorAll('.yourClassName')
+  // .forEach(el => { el.style.transform = 'rotate(15deg)'; });
+
+}
